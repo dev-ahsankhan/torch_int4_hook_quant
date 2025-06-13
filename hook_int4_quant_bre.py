fde63232fb52e4511e627fce6bf0c210bd3c7832
dev-ahsankhan <dev.ahsankhan@gmail.com>
2025-06-13 18:24:51 +0200

    Introduced POST FORWARD HOOK in 4-bit quantization with brevitas.

---
+++ hook_int4_quant_bre.py
@@ -0,0 +1,139 @@
+import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import time
+from torch.utils.cpp_extension import load
+
+import brevitas.nn as qnn
+from brevitas.quant import Int32Bias
+
+# Convert MNIST Image Files into a Tensor of 4-Dimensions (# of images, Height, Width, Color Channels)
+transform = transforms.ToTensor()
+# Train Data
+train_data = datasets.MNIST(root='./cnn_data', train=True, download=True, transform=transform)
+# Test Data
+test_data = datasets.MNIST(root='./cnn_data', train=False, download=True, transform=transform)
+
+# Create a small batch size for images...let's say 10
+train_loader = DataLoader(train_data, batch_size=10, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=10, shuffle=False)
+
+axx_mult = 'mul8s_acc'
+axx_conv2d_kernel = load(name='PyInit_conv2d_'+axx_mult, sources=["/scratch-local/khan/low_bit_quantization/nvidia_quant/adapt/adapt/cpu-kernels/axx_conv2d.cpp"], extra_cflags = ['-DAXX_MULT=' + axx_mult + ' -march=native -fopenmp -O3' ], extra_ldflags=['-lgomp'], verbose=True)
+# Define the approximate multiplier hook
+def approx_multiplier_hook(module, input, output):
+    # Simulate the effect of a 4-bit quantization for weights and activations
+    print("Inside hook")
+
+    act_input = input[0].int_repr()
+    weight = module.weight.int_repr()
+
+    breakpoint()
+
+    # Use the act_scale for any necessary scaling in the custom kernel
+    # Adjust custom kernel call as needed to conform with how it should handle int8/int4 data
+    
+    # Call your custom kernel; ensure that the kernel can handle int8 data
+    custom_output = axx_conv2d_kernel.forward(int4_act, int4_weight, tuple(module.kernel_size), tuple(module.stride), tuple(module.padding))
+    
+    return output
+
+
+
+class QuantWeightActBiasLeNet(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.quant_inp = qnn.QuantIdentity(bit_width=4, min_val=0.0, max_val=1.0, return_quant_tensor=True)
+        self.conv1 = qnn.QuantConv2d(1, 6, 3, bias=True, weight_bit_width=4, bias_quant=Int32Bias)
+        self.relu1 = qnn.QuantReLU(bit_width=4, return_quant_tensor=True)
+        self.conv2 = qnn.QuantConv2d(6, 16, 3, bias=True, weight_bit_width=4, bias_quant=Int32Bias)
+        self.relu2 = qnn.QuantReLU(bit_width=4, return_quant_tensor=True)
+        self.fc1   = qnn.QuantLinear(5*5*16, 120, bias=True, weight_bit_width=4, bias_quant=Int32Bias)
+        self.relu3 = qnn.QuantReLU(bit_width=4, return_quant_tensor=True)
+        self.fc2   = qnn.QuantLinear(120, 84, bias=True, weight_bit_width=4, bias_quant=Int32Bias)
+        self.relu4 = qnn.QuantReLU(bit_width=4, return_quant_tensor=True)
+        self.fc3   = qnn.QuantLinear(84, 10, bias=True, weight_bit_width=4, bias_quant=Int32Bias)
+
+    def forward(self, x):
+        out = self.quant_inp(x)
+        # breakpoint()
+        out = self.relu1(self.conv1(out))
+        out = F.max_pool2d(out, 2)
+        out = self.relu2(self.conv2(out))
+        out = F.max_pool2d(out, 2)
+        # out = out.view(out.shape[0], -1)
+        out = out.view(-1, 16*5*5)
+        breakpoint()
+        out = self.relu3(self.fc1(out))
+        out = self.relu4(self.fc2(out))
+        out = self.fc3(out)
+        return out
+
+torch.manual_seed(41)
+model = QuantWeightActBiasLeNet()
+
+print(model)
+# breakpoint()
+model.conv1.register_forward_hook(approx_multiplier_hook)
+
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+
+start_time = time.time()
+
+# Create Variables To Tracks Things
+epochs = 1
+train_losses = []
+test_losses = []
+train_correct = []
+test_correct = []
+
+# For Loop of Epochs
+for i in range(epochs):
+  trn_corr = 0
+  tst_corr = 0
+
+
+  # Train
+  for b,(X_train, y_train) in enumerate(train_loader):
+    b+=1 # start our batches at 1
+    y_pred = model(X_train) # get predicted values from the training set. Not flattened 2D
+    loss = criterion(y_pred, y_train) # how off are we? Compare the predictions to correct answers in y_train
+
+    predicted = torch.max(y_pred.data, 1)[1] # add up the number of correct predictions. Indexed off the first point
+    batch_corr = (predicted == y_train).sum() # how many we got correct from this batch. True = 1, False=0, sum those up
+    trn_corr += batch_corr # keep track as we go along in training.
+
+    # Update our parameters
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+
+    # Print out some results
+    if b%600 == 0:
+      print(f'Epoch: {i}  Batch: {b}  Loss: {loss.item()}')
+
+  train_losses.append(loss)
+  train_correct.append(trn_corr)
+
+
+  # Test
+  with torch.no_grad(): #No gradient so we don't update our weights and biases with test data
+    for b,(X_test, y_test) in enumerate(test_loader):
+      y_val = model(X_test)
+      predicted = torch.max(y_val.data, 1)[1] # Adding up correct predictions
+      tst_corr += (predicted == y_test).sum() # T=1 F=0 and sum away
+
+
+  loss = criterion(y_val, y_test)
+  test_losses.append(loss)
+  test_correct.append(tst_corr)
+
+
+
+current_time = time.time()
+total = current_time - start_time
+print(f'Training Took: {total/60} minutes!')
